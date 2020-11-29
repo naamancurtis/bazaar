44ef59d67718dee0a4b9ae08d8dec12aabe72e06
Naaman <naaman.the.dev@gmail.com>
2020-11-29 16:37:08 +0000

    ci(docker): create Dockerfile for builds

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,10 +1,19 @@
 use config::{Config, File};
 use serde::Deserialize;
+use std::convert::{TryFrom, TryInto};
+use std::env::var;
+use std::fmt;
 
 #[derive(Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
+}
+
+#[derive(Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
 }
 
 #[derive(Deserialize)]
@@ -16,9 +25,29 @@ pub struct DatabaseSettings {
     pub database_name: String,
 }
 
+pub enum Environment {
+    Local,
+    CI,
+    Production,
+}
+
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let mut settings = Config::default();
-    settings.merge(File::with_name("configuration"))?;
+    let base_path = std::env::current_dir().expect("failed to determine current directory");
+    let configuration_directory = base_path.join("configuration");
+
+    settings.merge(File::from(configuration_directory.join("base")).required(true))?;
+
+    let environment: Environment = var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("failed to parse APP_ENVIRONMENT");
+
+    settings
+        .merge(File::from(configuration_directory.join(environment.as_str())).required(true))?;
+
+    settings.merge(config::Environment::with_prefix("app").separator("__"))?;
+
     settings.try_into()
 }
 
@@ -41,5 +70,39 @@ impl DatabaseSettings {
             "postgres://{}:{}@{}:{}",
             self.username, self.password, self.host, self.port,
         )
+    }
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::CI => "ci",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl fmt::Display for Environment {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let string = match self {
+            Environment::Local => "local",
+            Environment::CI => "ci",
+            Environment::Production => "production",
+        };
+        write!(f, "{}", string)
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "ci" => Ok(Self::CI),
+            "production" => Ok(Self::Production),
+            other => Err(format!("{} is not a supported environment", other)),
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use bazaar::{
     build_app, get_configuration,
     telemetry::{generate_subscriber, init_subscriber},
 };
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 
 #[actix_rt::main]
@@ -12,11 +12,16 @@ async fn main() -> std::io::Result<()> {
 
     let configuration = get_configuration().expect("failed to read configuration");
 
-    let connection = PgPool::connect(&configuration.database.generate_connection_string())
+    let connection = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect(&configuration.database.generate_connection_string())
         .await
         .expect("failed to connect to database");
 
-    let addr = format!("127.0.0.1:{}", configuration.application_port);
+    let addr = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(addr)?;
 
     build_app(listener, connection)?.await
