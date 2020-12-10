dc293a65ac8ab5c4126a21b12cba9098f2ebd567
Naaman <naaman.the.dev@gmail.com>
2020-12-10 19:02:07 +0000

    ci(pipeline): try and fix build failing

--- src/auth/authenticate.rs
+++ src/auth/authenticate.rs
@@ -2,7 +2,6 @@ use anyhow::Result;
 use argon2::{self, Config, ThreadMode, Variant, Version};
 use lazy_static::lazy_static;
 use std::env::var;
-use tracing::warn;
 
 lazy_static! {
     pub static ref SECRET_KEY: String = var("SECRET_KEY").expect("no secret key found");
@@ -40,9 +39,7 @@ lazy_static! {
 }
 
 pub fn hash_password(password: &str) -> Result<String> {
-    warn!("attempting to hash password now");
     let hash = argon2::hash_encoded(password.as_bytes(), SALT.as_bytes(), &CONFIG)?;
-    warn!("returning hashed pw now");
     Ok(hash)
 }
 
