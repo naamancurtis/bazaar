52bb52aa814e62533e7f627363e73eac70cbfd9a
Naaman <naaman.the.dev@gmail.com>
2021-02-13 16:48:48 +0000

    feat(telemetry): update to use tracing-sprout

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ use opentelemetry_semantic_conventions::resource::{
     DEPLOYMENT_ENVIRONMENT, SERVICE_NAME, SERVICE_NAMESPACE,
 };
 use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
 use tracing_opentelemetry::OpenTelemetryLayer;
+use tracing_sprout::TrunkLayer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 use sqlx::postgres::PgPoolOptions;
@@ -25,7 +25,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>
     // @TODO Work out how to get OTEL metrics working
     let env_filter =
         EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(String::from("info")));
-    let formatting_layer = BunyanFormattingLayer::new(app_name.to_string(), std::io::stdout);
+    let formatting_layer = TrunkLayer::new(app_name.to_string(), std::io::stdout);
     LogTracer::init().expect("failed to attach logs to tracing");
 
     global::set_text_map_propagator(TraceContextPropagator::new());
@@ -42,7 +42,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>
     let otel_layer = OpenTelemetryLayer::new(tracer);
     let registry = Registry::default()
         .with(env_filter)
-        .with(JsonStorageLayer)
         .with(formatting_layer)
         .with(otel_layer);
     set_global_default(registry)?;
